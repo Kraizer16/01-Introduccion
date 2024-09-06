326927912084f4f0dec2b42adea105e1e3018dde
Kraizer16 <marlonacg1603@gmail.com>
2024-09-06 13:06:36 -0500

    Delete Convertidor_minutos.py

--- Convertidor_minutos.py
+++
@@ -1,7 +0,0 @@
-s = float(input("Ingrese la cantidad de segundos"))
-h = s // 3600
-m = (s % 3600) // 60
-sc = s % 60
-print (f"La cantidad de horas son:  {h:.0f} ")
-print (f"La cantidad de minutos son: {m:.0f} ")
-print (f"La cantidad de segundos son: {sc:.0f} ")
